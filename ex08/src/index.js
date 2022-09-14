228635acfaf1b3a6fce9f2e89b1e6b795726488a
Ajla Fijuljanin <ajlafi@maestralsolutions.com>
2022-09-14 14:41:41 +0200

    Repo10

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -4,19 +4,7 @@ function myObject() {
     E: 2.71828
     };
     // Only change code below this line
-
-
-    let obj = {
-        name:"Arena",
-        review:"Awesome"
-        };
-        Object.freeze(obj);
-        obj.review = "bad"; // will be ignored. Mutation not allowed
-        obj.newProp = "Test"; // will be ignored. Mutation not allowed
-        console.log(obj); // { name: "Arena", review:"Awesome"}
-
-        Freeze the MATH_CONSTANTS object so that no one is able to alter the value of E, add, or delete properties
-
+    Object.freeze(MATH_CONSTANTS);
     // Only change code above this line
     try {
     MATH_CONSTANTS.E = 23;
@@ -27,4 +15,3 @@ function myObject() {
     }
     const E = myObject();
     module.exports = myObject;
-    
--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,14 +1,17 @@
 function myFunction(myObj, checkProp) {
     // Only change code below this line
-    var myObj = {
-        "title": "Titanic",
-        "song": "My Heart Will Go On",
-        "genre": "drama"
-    };
-    myObj.hasOwnProperty("title"); // true
-    myObj.hasOwnProperty("actor"); // not found     
-    return "Titanic";
-    console.log(myFunction(myObj, checkProp));
+    
+    if (myObj.hasOwnProperty(checkProp)) {
+        return myObj[checkProp];
+    } else {
+        return "Not found"
+    }
     // Only change code above this line
 }
+
+console.log(myFunction({
+    "title": "Titanic",
+    "song": "My Heart Will Go On",
+    "genre": "drama"
+}, 'title'))
 module.exports = myFunction;
